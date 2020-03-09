70c9080fb8aa4c63ab33d80afcfbcfbefd7e07cd
seepiol <seepiol@protonmail.com>
2020-03-09 19:48:57 +0100

    Added italian 'today new cases'. Documented the data fetching process

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# COVIDTracker, 2020
+# Simple CLI tool written in python to track the spreading of the SARS-CoV-2 outbreak
+
 import csv
 import datetime
 import requests
@@ -10,6 +13,11 @@ print("\033[91m\033[1m== ☣ COVID-19 - SARS-CoV-2 Spread Tracker ☣ ==\033[0m"
 print("\nFetching latest data.....", end="")
 
 #### INTERNATIONAL DATA
+# the JHU CSSE doesn't provide an official REST API; They upload once a day the situation on github, each day in a
+# different file. Because the filename is the current date, this program get the current and request the currentdate.csv
+# file. if the http response isn't 200, we assume that the current day file isn't available yet, and we try to decrease
+# the currentdate to fetch the last available file. The program save the file in a local .csv file
+
 i = 0
 date = datetime.date.today()
 date = date.strftime("%m-%d-%Y")
@@ -35,13 +43,14 @@ with open("covid.csv", "w") as file:
 
 
 #### ITALIAN DATA
+# The procedure to get italian data is more simple. The daily repost is saved on a .json file. All the daily data are
+# saved in the same file, so it's enough to get the json file and convert it in a dict with the json module.
+
 it_json_url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
 it_json = json.loads(requests.get(it_json_url).text)
 it_current_data = it_json[len(it_json)-1]
 
-
 print(" OK \n")
-
 
 if len(sys.argv) > 1 :
     region = sys.argv[1].title()
@@ -86,6 +95,7 @@ if region == "Italy":
 ITALY REPORT
 
 🛑 \033[94m\033[1mTOTAL CONFIRMED CASES = {it_current_data.get("totale_casi")}\033[0m
+➕ \033[94m\033[1mTODAY NEW CASES = {it_current_data.get("nuovi_attualmente_positivi")}\033[0m
 🌡  \033[0;36m\033[1mTOTAL CURRENT INFECTED CASES = {it_current_data.get("totale_attualmente_positivi")}\033[0m
 🔬 \033[1;36m\033[1mCURRENT INFECTED CASES IN ICU* = {it_current_data.get("terapia_intensiva")}\033[0m
 🏥 \033[1;36m\033[1mCURRENT INFECTED CASES HOSPITALISED = {it_current_data.get("totale_ospedalizzati")}\033[0m
@@ -119,7 +129,10 @@ Last update: {date}
 
 print(
     """
-Data source:
+More information about the SARS-CoV-2 outbreak and COVID-19 disease here: 
+\033[4mhttps://github.com/seepiol/COVIDTracker#about-covid-19\033[0m
+
+Data sources:
 World: \033[4mhttps://github.com/CSSEGISandData/COVID-19\033[0m
 Italy: \033[4mhttps://github.com/pcm-dpc/COVID-19\033[0m
 """
