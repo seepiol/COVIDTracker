ff76a4312d978e0800e11b12e537261392ffd0b5
seepiol <seepiol@protonmail.com>
2020-06-06 17:41:27 +0200

    removed world stuff, now just italy. fixed json reading. updated README.md

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # COVID TRACKER
-Simple COVID-19 tracker written in python3. [DISCONTINUED]
+
+Simple COVID-19 tracker written in python3. 
 
 ![COVIDTracker](https://user-images.githubusercontent.com/60071372/75629114-91318e00-5bdf-11ea-8907-626c48308853.gif)
 
 ## Data Sources
-The data fetching process is different from country to country.
-Generally the data are obtained through the CSV files posted daily on [github](https://github.com/CSSEGISandData/COVID-19) by John Hopkins University <br/>
-The italian data are fetched from the Italian Civil Protection (Presidenza del Consiglio dei Ministri - Dipartimento della Protezione Civile) [github repository](https://github.com/pcm-dpc/COVID-19) and there are more accurate information like the collocation of the infected etc.
 
-When the world situation is selected, all the data, included the italian ones, come from the JHU repo.
+The italian data are fetched from the Italian Civil Protection (Presidenza del Consiglio dei Ministri - Dipartimento della Protezione Civile) [github repository](https://github.com/pcm-dpc/COVID-19) and there are more accurate information like the collocation of the infected etc.
 
 ## About COVID-19
 
 For more information about the COVID-19, SARS-CoV-2 and 2019-2020 Coronavirus Outbreak:
+
 1. [WHO](https://www.who.int/emergencies/diseases/novel-coronavirus-2019)
 2. [CDC](https://www.cdc.gov/coronavirus/2019-nCoV/)
 3. [ECDC](https://www.ecdc.europa.eu/en/novel-coronavirus-china)
@@ -21,14 +20,18 @@ For more information about the COVID-19, SARS-CoV-2 and 2019-2020 Coronavirus Ou
 6. [COVID-19 Global Cases by Johns Hopkins CSSE](https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6)
 7. [IT] [Italian minister of health](http://www.salute.gov.it/nuovocoronavirus)
 
-
 ## How To Install
+
 ### Install GIT
+
 if you have a debian-based distro
+
 ```
 sudo apt install git
 ```
+
 if you have an arch-based distro
+
 ```
 sudo pamac install git
 ```
@@ -36,29 +39,24 @@ sudo pamac install git
 if you have windows, click the ["Download zip button"](https://github.com/seepiol/COVIDTracker/archive/master.zip)
 
 ### Install Dependencies
+
 Extract the files from master.zip and open your terminal (or the command prompt) into the "master" folder.
 Verify to be in the right directory, and type
+
 ```
 pip install -r requirements.txt
 ```
+
 This will install all the python modules that the program need
 
 ## Run The Program
+
 In the same folder, type
+
 ```
 python3 main.py
 ```
-insert the contry that you want and view the spreading data.
 
-If you want to write the country as a cli argument, type for example
-```
-python3 main.py france
-```
+the italian data will be displayed
 
-![CLIarguments](https://user-images.githubusercontent.com/60071372/75629129-a0b0d700-5bdf-11ea-8185-ff837d315e53.gif)
-
-### Mail Sender
-A version of this program that sends the reports with the SMTP protocol (e-mail) will be uploaded soon.
-
-
-**Thanks to JHU and Dipartimento della Protezione Civile for the data publication**
+**Thanks to Dipartimento della Protezione Civile for the data publication**
--- main.py
+++ main.py
@@ -1,46 +1,15 @@
 # COVIDTracker, 2020
 # Simple CLI tool written in python to track the spreading of the SARS-CoV-2 outbreak
 
-import csv
-import datetime
+
 import requests
 import sys
 import json
 
 spec_data_source = ["Italy"]
 
-print("\033[91m\033[1m== ☣ COVID-19 - SARS-CoV-2 Spread Tracker ☣ ==\033[0m")
+print("\033[91m\033[1m== ☣ COVID-19 - SARS-CoV-2 Italy Spread Tracker ☣ ==\033[0m")
 print("\nFetching latest data.....", end="")
-
-#### INTERNATIONAL DATA
-# the JHU CSSE doesn't provide an official REST API; They upload once a day the situation on github, each day in a
-# different file. Because the filename is the current date, this program get the current and request the currentdate.csv
-# file. if the http response isn't 200, we assume that the current day file isn't available yet, and we try to decrease
-# the currentdate to fetch the last available file. The program save the file in a local .csv file
-
-i = 0
-date = datetime.date.today()
-date = date.strftime("%m-%d-%Y")
-url = (
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
-    + date
-    + ".csv"
-)
-daily_report = requests.get(url)
-while daily_report.status_code != 200:
-    i += 1
-    date = datetime.date.today() - datetime.timedelta(i)
-    date = date.strftime("%m-%d-%Y")
-    url = (
-        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
-        + date
-        + ".csv"
-    )
-    daily_report = requests.get(url)
-
-with open("covid.csv", "w") as file:
-    file.write(str(daily_report.content.decode("ascii")))
-
 
 #### ITALIAN DATA
 # The procedure to get italian data is more simple. The daily repost is saved on a .json file. All the daily data are
@@ -52,41 +21,7 @@ it_current_data = it_json[len(it_json)-1]
 
 print(" OK \n")
 
-if len(sys.argv) > 1 :
-    region = sys.argv[1].title()
-    print(f"Selected {region}")
-else:
-    region = input("Insert country or region (world for all): ").title()
-
-if len(region) == 2:
-    region = region.upper()
-if region == "*":
-    region = True
-
-total_confirmed = 0
-total_deaths = 0
-total_recovered = 0
-
-states = []
-confirmed = 0
-deaths = 0
-recovered = 0
-
-with open("covid.csv") as daily_report:
-    parsed_daily_report = csv.reader(daily_report, delimiter=",")
-    for row in parsed_daily_report:
-        if region == "Italy":
-            break
-        elif (region.lower() in row[0].lower() or region.lower() in row[1].lower() or region == "World"):
-            if row[0] == "Province/State":
-                pass
-            else:
-                states.append(row[0])
-                confirmed += int(row[3])
-                deaths += int(row[4])
-                recovered += int(row[5])
-
-
+region = "Italy"
 
 if region == "Italy":
     print(f"""
@@ -95,8 +30,8 @@ if region == "Italy":
 ITALY REPORT
 
 🛑 \033[94m\033[1mTOTAL CONFIRMED CASES = {it_current_data.get("totale_casi")}\033[0m
-➕ \033[94m\033[1mTODAY NEW CASES = {it_current_data.get("nuovi_attualmente_positivi")}\033[0m
-🌡  \033[0;36m\033[1mTOTAL CURRENT INFECTED CASES = {it_current_data.get("totale_attualmente_positivi")}\033[0m
+➕ \033[94m\033[1mTODAY NEW CASES = {it_current_data.get("nuovi_positivi")}\033[0m
+🌡  \033[0;36m\033[1mTOTAL CURRENT INFECTED CASES = {it_current_data.get("terapia_intensiva")+it_current_data.get("totale_ospedalizzati")+it_current_data.get("isolamento_domiciliare")}\033[0m
 🔬 \033[1;36m\033[1mCURRENT INFECTED CASES IN ICU* = {it_current_data.get("terapia_intensiva")}\033[0m
 🏥 \033[1;36m\033[1mCURRENT INFECTED CASES HOSPITALISED = {it_current_data.get("totale_ospedalizzati")}\033[0m
 🏠 \033[1;36m\033[1mCURRENT INFECTED CASES IN HOME ISOLATION = {it_current_data.get("isolamento_domiciliare")}\033[0m
@@ -109,31 +44,12 @@ Last update: {it_current_data.get("data")}
 ====================================================
 """)
 
-else:
-    print(
-    f"""
-====================================================
-\033[0;31m\033[1m☣ COVID-19 ☣\033[0m
-{region.upper()} REPORT
-({len(states)} states)
-🛑 \033[94m\033[1mCONFIRMED CASES = {confirmed}\033[0m
-💀 \033[0;31m\033[1mDEATHS = {deaths}\033[0m
-✅ \033[92m\033[1mRECOVERED CASES = {recovered}\033[0m
-
-Last update: {date}
-====================================================
-"""
-)
-
-
-
 print(
     """
 More information about the SARS-CoV-2 outbreak and COVID-19 disease here: 
 \033[4mhttps://github.com/seepiol/COVIDTracker#about-covid-19\033[0m
 
 Data sources:
-World: \033[4mhttps://github.com/CSSEGISandData/COVID-19\033[0m
 Italy: \033[4mhttps://github.com/pcm-dpc/COVID-19\033[0m
 """
 )
